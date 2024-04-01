4ba11ef48311d96ee9fc6637c4abfffe7801b37c
Henrique Sanchez <henriquealcici@gmail.com>
2024-04-01 23:40:27 +0000

    Variáveis no JavaScript

---
+++ javascript/variaveis.js
@@ -0,0 +1,8 @@
+var variavel_1 = 2; //antiga
+let variavel_2 = 5; //preferencial
+const variavel_3 = "Olá mundo cruel" ; //atribui dados apenas uma vez. (constante)
+
+console.log(variavel_3);
+console.log("Olá Mundo");
+console.log("Mensagem", variavel_3);
+console.log("Mensagem" + variavel_3);
